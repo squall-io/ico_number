d3733f17531076610f28e15c5e1964b4821077b2
Salathiel Genese <salathielgenese@gmail.com>
2020-04-21 13:14:53 -0400

    Refactor to support i18n

--- lib/how-to/screen.how-to.dart
+++ lib/how-to/screen.how-to.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
 import 'package:ico_number/widget.factory.dart';
 
 
@@ -14,57 +15,86 @@ class HowToScreen extends StatefulWidget {
 
 class _HowToScreenState extends State<HowToScreen> {
 
+  static const _divider = Divider();
+
   static final _random = Random();
 
-  List<_Step> _steps;
+  List<int> _digits;
+  int _difference;
+  int _digitsSum;
+  int _number;
 
   _HowToScreenState() {
-    final digits = <int>[];
-    var digitCount = 2 + _random.nextInt(3);
-
-    do {
-      digits.add(_random.nextInt(9));
-      if (0 == digits.first) digits.removeAt(0);
-    } while (digitCount > digits.length);
-
-    final number = digits
-      .map((digit) => digit * pow(10, --digitCount))
-      .reduce((hay, number) => hay + number);
-    final sum = digits.reduce((stack, digit) => stack + digit);
-    final difference = number - sum;
-
-    _steps = [
-      _Step('Imagin a whole number with 2+ digits', 'Example: $number'),
-      _Step('Sum its digits', '${ digits.join(' + ') } = $sum'),
-      _Step('Substract the sum from the number', '$number - $sum = $difference'),
-      _Step('Check result\'s matching icon', 'Match icon for $difference'),
-      _Step('Let me guess witch icon it was', 'That\'s it !'),
-    ];
+    _number = 10 + _random.nextInt(pow(10, 2 + _random.nextInt(3)) - 1);
+    _digits = _number.toString().split('').map(int.parse).toList();
+    _digitsSum = _digits.reduce((a, b) => a + b);
+    _difference = _number - _digitsSum;
   }
 
   @override
   Widget build(BuildContext context) =>
     WidgetFactory.getScaffold(
-      title: "How It Works",
       child: ListView(
-        children: _steps,
+        children: <Widget>[
+          _Step(
+            description: 'step.description.imagine',
+            example: 'step.example.imagine',
+            parameters: { 'number': _number },
+          ),
+          _divider,
+          _Step(
+            description: 'step.description.sum-digits',
+            example: '${ _digits?.join(' + ' ) } = $_digitsSum',
+            isPlain: true,
+          ),
+          _divider,
+          _Step(
+            description: 'step.description.substract',
+            example: '$_number - $_digitsSum = $_difference',
+            isPlain: true,
+          ),
+          _divider,
+          _Step(
+            description: 'step.description.look-icon',
+            example: 'step.example.look-icon',
+            parameters: { 'difference': _difference },
+          ),
+          _divider,
+          _Step(
+            description: 'step.description.remember-icon',
+          ),
+          _divider,
+          _Step(
+            description: 'step.description.guess-icon',
+            example: 'step.description.thank-you',
+          ),
+        ],
       ),
+      title: FlutterI18n.translate(context, 'screen.title.how-to'),
     );
 
 }
 
+// ignore: must_be_immutable
 class _Step extends StatelessWidget {
 
-  final String _instruction;
-  final String _example;
+  Map<String, String> _parameters;
+  String _description;
+  String _example;
+  bool _isPlain;
 
-  const _Step(this._instruction, this._example, {Key key}) : super(key: key);
+  _Step({ Key key, @required String description, String example, bool isPlain: false, Map<String, dynamic> parameters}) : super(key: key) {
+    _parameters = null == parameters ? null : parameters.map((key, value) => MapEntry(key, '$value'));
+    _description = description;
+    _example = example;
+    _isPlain = isPlain;
+  }
 
   @override
   Widget build(BuildContext context) =>
     ListTile(
-      subtitle: Text(_example),
-      title: Text(_instruction),
+      title: I18nText(_description),
+      subtitle: null != _example ? _isPlain ? Text(_example) : I18nText(_example, translationParams: _parameters) : null,
     );
 
 }
